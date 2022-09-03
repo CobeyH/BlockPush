71710303f73547e7904032c917c2d6a57da3086e
Cobey Hollier <cobey.hollier@mac.com>
2022-09-02 18:33:49 -0700

    CH: Adding chunk with finish line at end

--- Assets/Scripts/ChunkSpawner.cs
+++ Assets/Scripts/ChunkSpawner.cs
@@ -6,16 +6,22 @@ public class ChunkSpawner : MonoBehaviour
 {
     [SerializeField]
     private GameObject chunkPrefab;
+
+    [SerializeField]
+    private GameObject endChunk;
+
     public int levelSize = 1;
 
-    // The game is rendered in chunks. Each chunk is 10 x 10 and will contain objects on it. A checkpoint may exist after each checkpoint.
+    // The game is rendered in chunks. Each chunk is 10 x 10 and will contain objects on it. A checkpoint may exist after each chunk.
     void Start()
     {
+        Vector3 position = new Vector3(0, -0.75f, 0);
         for (int i = 0; i < levelSize; i++)
         {
-            Vector3 position = new Vector3(0, -0.75f, chunkPrefab.transform.localScale.z * i);
             Instantiate(chunkPrefab, position, Quaternion.identity);
+            position += new Vector3(0, 0, chunkPrefab.transform.localScale.z);
         }
+        Instantiate(endChunk, position, Quaternion.identity);
     }
 
     // Update is called once per frame
