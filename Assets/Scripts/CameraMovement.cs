5545bfe5e4e366ec8020d34e038e8426e0df73f5
Cobey Hollier <cobey.hollier@mac.com>
2022-09-02 19:19:16 -0700

    CH: Implementing camera catchup in script

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,12 +5,36 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
 
-    public int speedModifier = 5;
+    public int speedModifier = 3;
+    [SerializeField]
+    float trailingModifier = 0.3f;
+
+    [SerializeField]
+    GameObject player;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 positionOffset = new Vector3(0, 0, speedModifier * Time.deltaTime);
+        float offsetFactor = CalculatePositionOffset();
+        Vector3 positionOffset = new Vector3(0, 0, offsetFactor);
         gameObject.transform.position += positionOffset;
+    }
+
+    // Calculates how far the camera should move along the Z-Axis.
+    // If the player is far ahead of the camera, the camera will speed up in order to catch up.
+    float CalculatePositionOffset()
+    {
+        float distanceFactor = DistanceToPlayer() * trailingModifier * Time.deltaTime;
+        float speedFactor = speedModifier * Time.deltaTime;
+        return Mathf.Max(distanceFactor, speedFactor);
+    }
+
+    // Calculates the distance from the camera to the player along the Z-axis
+    float DistanceToPlayer()
+    {
+        float camPosZ = gameObject.transform.position.z;
+        float playerPosZ = player.transform.position.z;
+
+        return playerPosZ - camPosZ;
     }
 }
