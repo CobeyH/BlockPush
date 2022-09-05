4b8c5f2e4412bed6179d5e91da15ef850a6c1e8a
Cobey Hollier <cobey.hollier@mac.com>
2022-09-05 10:23:14 -0700

    CH: Starting object generation script

--- Assets/Scripts/ChunkSpawner.cs
+++ Assets/Scripts/ChunkSpawner.cs
@@ -5,44 +5,66 @@ using UnityEngine;
 public class ChunkSpawner : MonoBehaviour
 {
     [SerializeField]
-    private GameObject chunkPrefab;
+    GameObject chunkPrefab;
 
     [SerializeField]
-    private GameObject endChunk;
+    GameObject endChunk;
 
     [SerializeField]
-    private GameObject checkpoint;
-
+    Texture2D[] maps;
     [SerializeField]
-    private int CheckpointInterval = 2;
+    ColorToPrefab[] colorMappings;
 
-    public int levelSize = 1;
 
     // The game is rendered in chunks. Each chunk is 10 x 10 and will contain objects on it. A checkpoint may exist after each chunk.
     void Start()
     {
         Vector3 position = new Vector3(0, -0.75f, 0);
-        // Build X chunks where X equals the level size.
-        for (int i = 1; i < levelSize + 1; i++)
+        // Build a chunk for each map in the array
+        for (int i = 0; i < maps.Length; i++)
         {
-            position = BuildTile(chunkPrefab, position);
-            // Add a checkpoint between each X chunks where X is the checkpoint interval.
-            if (i % CheckpointInterval == 0)
-            {
-                position = BuildTile(checkpoint, position);
-            }
+            // The build position will be set to the center of the tile.
+            position += new Vector3(0, 0, maps[i].height / 2);
+            BuildTile(maps[i], position);
+            position += new Vector3(0, 0, maps[i].height / 2);
         }
-        // Add the end chunk with the goal.
-        BuildTile(endChunk, position);
     }
 
     // Take a prefab and update the position so that they generate connected in a straight line.
-    Vector3 BuildTile(GameObject tile, Vector3 origin)
+    void BuildTile(Texture2D map, Vector3 position)
     {
-        float tileLength = tile.transform.localScale.z;
-        origin.z += tileLength / 2f;
-        Instantiate(tile, origin, Quaternion.identity);
-        origin.z += tileLength / 2f;
-        return origin;
+        // Build ground
+        GameObject groundTile = Instantiate(chunkPrefab, position, Quaternion.identity);
+        groundTile.transform.localScale = new Vector3(map.width, 0.5f, map.height);
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int y = 0; y < map.height; y++)
+            {
+                Color pixel = map.GetPixel(x, y);
+                GenerateObject(pixel, position);
+            }
+        }
+        // float tileLength = tile.transform.localScale.z;
+        // origin.z += tileLength / 2f;
+        // Instantiate(tile, origin, Quaternion.identity);
+        // origin.z += tileLength / 2f;
+        // return origin;
+    }
+
+    void GenerateObject(Color pixel, Vector3 drawPosition)
+    {
+        if (pixel.a == 0)
+        {
+            return;
+        }
+
+        foreach (ColorToPrefab colorMapping in colorMappings)
+        {
+            if (colorMapping.color == pixel)
+            {
+                Instantiate(colorMapping.prefab, drawPosition, Quaternion.identity);
+            }
+        }
+
     }
 }
